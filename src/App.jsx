f8059d7ef97383c4721f64654fd5a7806e9f8964
Himanshu <himanshuparodiya@gmail.com>
2023-07-23 14:07:25 +0530

    Dark and light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+  const theme = useSelector((state) => state.theme.appTheme);
+
   return (
-    <div className="app">
+    <div className={` app ${theme ? "light" : ""}`}>
       <Navbar />
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,32 @@
 import React, { useRef, useState } from "react";
 import "./Navbar.css";
-import { BsFillMoonStarsFill } from "react-icons/bs";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useDispatch, useSelector } from "react-redux";
+import { RxCross2 } from "react-icons/rx";
+import { setTheme } from "../../Store/Slice/ThemeSlice";
 
 const Navbar = () => {
+  const theme = useSelector((state) => state.theme.appTheme);
+  //   console.log(theme);
+  const [showHamburger, setShowHamburger] = useState(true);
+  const dispatch = useDispatch();
   const hamburgerIcon = useRef();
   const handleHamburgerMenu = () => {
     hamburgerIcon.current.classList.toggle("show");
+    setShowHamburger(!showHamburger);
   };
 
   return (
-    <header className="header__container container">
-      <nav className="navbar__container">
+    <header className="header__container container ">
+      <nav className={`navbar__container ${theme ? "light" : ""}`}>
         <div className="logo">HP</div>
         <div className="navbar__menu">
-          <div className="hamburgerMenu" onClick={handleHamburgerMenu}>
-            <GiHamburgerMenu />
+          <div
+            className={`hamburgerMenu ${theme ? "light" : ""}`}
+            onClick={handleHamburgerMenu}
+          >
+            {showHamburger ? <GiHamburgerMenu /> : <RxCross2 />}
           </div>
           <ul ref={hamburgerIcon} className="navbar__menuUl">
             <li className="navbar__menuLi">
@@ -40,8 +51,8 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <div className="theme">
-          <BsFillMoonStarsFill />
+        <div className="theme" onClick={() => dispatch(setTheme(!theme))}>
+          {theme ? <BsFillMoonStarsFill /> : <BsFillSunFill color="gold" />}
         </div>
       </nav>
     </header>
