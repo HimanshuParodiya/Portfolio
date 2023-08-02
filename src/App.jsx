36425a9b587cc58d54ac84288f06469b8e1d65c6
Himanshu <himanshuparodiya@gmail.com>
2023-08-02 12:50:09 +0530

    fix Scroll To top

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,26 @@ import { useSelector } from "react-redux";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import About from "./components/About/About";
 import Project from "./components/Projects/Project";
 import { useEffect } from "react";
 
 function App() {
   const theme = useSelector((state) => state.theme.appTheme);
+  const location = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+  }, [location]);
 
   return (
     <div className={`app ${theme ? "light" : ""}`}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Project />} />
-        </Routes>
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Project />} />
+      </Routes>
     </div>
   );
 }
