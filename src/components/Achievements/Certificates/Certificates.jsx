1f16ef472aa4888ede423059e6b2ab03c4d64f84
Himanshu <himanshuparodiya@gmail.com>
2023-12-07 12:55:30 +0530

    remove vertical timeline for certificates section

--- src/components/Achievements/Certificates/Certificates.jsx
+++ src/components/Achievements/Certificates/Certificates.jsx
@@ -15,39 +15,22 @@ const Certificates = () => {
   return (
     <div className="certificates__container">
       <h2 className="certificates__heading">Certificates</h2>
+      <div className="certificates__images">
+        <EachCertificate
+          link="https://www.hackerrank.com/certificates/7dfcba1d9c07"
+          certificateImage={cssCertificate}
+        />
 
-      <VerticalTimeline>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "transparent", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          //   date="2011 - present"
-          iconStyle={{ background: "#191825", color: "#845EC2" }}
-          icon={<LiaCertificateSolid style={{ scale: "1.3" }} />}
-        >
-          <EachCertificate certificateImage={cssCertificate} />
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "transparent" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          //   date="2011 - present"
-          iconStyle={{ background: "#191825", color: "#845EC2" }}
-          icon={<LiaCertificateSolid style={{ scale: "1.3" }} />}
-        >
-          <EachCertificate certificateImage={ReactCertificate} />
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "transparent", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          //   date="2011 - present"
-          iconStyle={{ background: "#191825", color: "#845EC2" }}
-          icon={<LiaCertificateSolid style={{ scale: "1.3" }} />}
-        >
-          <EachCertificate certificateImage={JavascriptCertificate} />
-        </VerticalTimelineElement>
-      </VerticalTimeline>
+        <EachCertificate
+          link="https://www.hackerrank.com/certificates/3e058d105bd2"
+          certificateImage={JavascriptCertificate}
+        />
+
+        <EachCertificate
+          link="https://www.hackerrank.com/certificates/d93047d83b38"
+          certificateImage={ReactCertificate}
+        />
+      </div>
     </div>
   );
 };
--- src/components/Achievements/Certificates/EachCertificate.jsx
+++ src/components/Achievements/Certificates/EachCertificate.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./EachCertificate.css";
+import { Link } from "react-router-dom";
 
-const EachCertificate = ({ certificateImage }) => {
+const EachCertificate = ({ link, certificateImage }) => {
   return (
     <div className="eachCertificate__container">
-      <img src={certificateImage} alt="" />
+      <a target="_blank" href={link}>
+        <img src={certificateImage} alt="certificate Image" />
+      </a>
     </div>
   );
 };
